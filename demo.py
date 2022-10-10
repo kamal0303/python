0a5339d3b0b97720e663609f5adee88e9e5b292e
kamal0303 <kamalsharma0308@gmail.com>
2022-10-10 23:02:09 +0530

    update

--- demo.py
+++ demo.py
@@ -1,4 +1,9 @@
 var_a = 5
 var_b = 25
 integer_division = var_b//var_a
+float_division = var_b/var_a
+modulo_division = var_b % var_a
+
 print(f"integer division: {var_b}/{var_a}={integer_division}")
+print(f"float division: {var_b}/{var_a}={float_division}")
+print(f"modulous division: {var_b}%{var_a}={modulo_division}")
