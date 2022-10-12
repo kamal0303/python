2ac8837438cab0692f510d6a43f167e049715360
kamal0303 <kamalsharma0308@gmail.com>
2022-10-12 22:29:40 +0530

    update

--- func_py.py
+++ func_py.py
@@ -1,11 +1,21 @@
-def disccalc(cp, discount):
-    discount = cp * (discount/100)
-    selling_price = cp - discount
-    return selling_price
+# def disccalc(cp, discount):
+#     discount = cp * (discount/100)
+#     selling_price = cp - discount
+#     return selling_price
 
 
-sp = disccalc(660, 30)
-print(sp)
+# sp = disccalc(660, 30)
+# print(sp)
 
-print = "Hi there"
-type(print)
+pi = 22/7
+rounded_pi = round(pi, 2)
+print(rounded_pi)
+
+z_four = 5 < 3
+print(z_four)
+
+z_five = not z_four
+print(z_five)
+
+shortest_string = ""
+print(len(shortest_string))
