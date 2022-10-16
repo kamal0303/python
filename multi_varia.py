21073547f0a79b15c9045cfd61ae0ed821eea56a
kamal0303 <kamalsharma0308@gmail.com>
2022-10-16 13:59:54 +0530

    update 16-10

---
+++ multi_varia.py
@@ -0,0 +1,9 @@
+firstName, middleName, surName = "kamal", "kishore", "sharma"
+print(f"{firstName}.{surName}@gmail.com")
+
+name, place, year_birth, weight = "kamal", "chandigarh", 1989, 52.0
+print(f'''
+        my name is: {name}
+        i live in: {place}
+        i was born in: {year_birth}
+        my weight is: {weight}''')
