f5949e7bdc3b5a6f9d93e1b20d22165c122bd12e
kamal0303 <kamalsharma0308@gmail.com>
2022-10-16 12:35:46 +0530

    update

--- string_func.py
+++ string_func.py
@@ -1,28 +1,40 @@
-# string1 = "datascientist"
-# # string1.casefold()
-# print(string1.count('a'))
-# print(string1.center())
+# # string1 = "datascientist"
+# # # string1.casefold()
+# # print(string1.count('a'))
+# # print(string1.center())
 
-# string to integer
-A = "50"
-print(f"Data type before: {type(A)}")
-A = int(A)
-print(f"Data Type After: {type(A)}")
+# # string to integer
+# A = "50"
+# print(f"Data type before: {type(A)}")
+# A = int(A)
+# print(f"Data Type After: {type(A)}")
 
-# float to integer
-A = 50.22
-print(f"Data type before: {type(A)}, value of A: {A}")
-A = str(A)
-print(f"Data type after: {type(A)}, value of A: {A}")
+# # float to integer
+# A = 50.22
+# print(f"Data type before: {type(A)}, value of A: {A}")
+# A = str(A)
+# print(f"Data type after: {type(A)}, value of A: {A}")
 
-# integer to float
-A = 50
-print(f"Data type before: {type(A)}, value of A: {A}")
-A = float(A)
-print(f"Data type after: {type(A)}, value of A: {A}")
+# # integer to float
+# A = 50
+# print(f"Data type before: {type(A)}, value of A: {A}")
+# A = float(A)
+# print(f"Data type after: {type(A)}, value of A: {A}")
 
-# integer to boolean
-A = -0
-print(f"Data type before: {type(A)}, value of A: {A}")
-A = bool(A)
-print(f"Data type after: {type(A)}, value of A: {A}")
+# # integer to boolean
+# A = ""
+# print(f"Data type before: {type(A)}, value of A: {A}")
+# A = bool(A)
+# print(f"Data type after: {type(A)}, value of A: {A}")
+
+# # empty string to integer
+# # A = " "
+# # print(f"data type before: {type(A)}, value of A: {A}")
+# # A = int(A)
+# # print(f"Data type after: {type(A)}, value of A: {A}")
+
+# # multi assignments of variable
+
+firstName, middleName, surName = "kamal", "kishore", "sharma"
+print(f"{firstName}.{surName}@gmail.com")
+# print(middleName)
