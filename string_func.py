16224806cb6f97462283b68124ea7da0f56533c4
kamal0303 <kamalsharma0308@gmail.com>
2022-11-06 14:16:19 +0530

    update

--- string_func.py
+++ string_func.py
@@ -1,39 +1,39 @@
-# # string1 = "datascientist"
-# # # string1.casefold()
-# # print(string1.count('a'))
-# # print(string1.center())
+# string1 = "datascientist"
+# # string1.casefold()
+# print(string1.count('a'))
+# print(string1.center())
 
-# # string to integer
-# A = "50"
-# print(f"Data type before: {type(A)}")
+# string to integer
+A = "50"
+print(f"Data type before: {type(A)}")
+A = int(A)
+print(f"Data Type After: {type(A)}")
+
+# float to integer
+A = 50.22
+print(f"Data type before: {type(A)}, value of A: {A}")
+A = str(A)
+print(f"Data type after: {type(A)}, value of A: {A}")
+
+# integer to float
+A = 50
+print(f"Data type before: {type(A)}, value of A: {A}")
+A = float(A)
+print(f"Data type after: {type(A)}, value of A: {A}")
+
+# integer to boolean
+A = ""
+print(f"Data type before: {type(A)}, value of A: {A}")
+A = bool(A)
+print(f"Data type after: {type(A)}, value of A: {A}")
+
+# empty string to integer
+# A = " "
+# print(f"data type before: {type(A)}, value of A: {A}")
 # A = int(A)
-# print(f"Data Type After: {type(A)}")
-
-# # float to integer
-# A = 50.22
-# print(f"Data type before: {type(A)}, value of A: {A}")
-# A = str(A)
 # print(f"Data type after: {type(A)}, value of A: {A}")
 
-# # integer to float
-# A = 50
-# print(f"Data type before: {type(A)}, value of A: {A}")
-# A = float(A)
-# print(f"Data type after: {type(A)}, value of A: {A}")
-
-# # integer to boolean
-# A = ""
-# print(f"Data type before: {type(A)}, value of A: {A}")
-# A = bool(A)
-# print(f"Data type after: {type(A)}, value of A: {A}")
-
-# # empty string to integer
-# # A = " "
-# # print(f"data type before: {type(A)}, value of A: {A}")
-# # A = int(A)
-# # print(f"Data type after: {type(A)}, value of A: {A}")
-
-# # multi assignments of variable
+# multi assignments of variable
 
 firstName, middleName, surName = "kamal", "kishore", "sharma"
 print(firstName)
---
+++ xxxx.py
@@ -0,0 +1,3 @@
+a = float(input("Enter Length: "))
+b = float(input("Enter Width: "))
+a = float(input("Enter height: "))
