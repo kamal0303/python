55d45b8a55079a2a7c4f8f278813bb2cb5443ebd
kamal0303 <kamalsharma0308@gmail.com>
2022-11-08 18:47:22 +0530

    update

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# makemigrations - create changes and store in a file
+# migrate - apply the pending changes created by makemigrations
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=120)
+    surname = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    phone = models.CharField(max_length=12)
+    message = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name + self.email
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 
 
@@ -8,6 +10,7 @@ def index(request):
         "variable1": "this is sent",
         "variable2": "welcome to the jungle"
     }
+    messages.success(request, "this is a test message")
     return render(request, 'index.html', context)
     # return HttpResponse("This is Homepage")
 
@@ -23,5 +26,16 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        contact = Contact(name=name, surname=surname, email=email,
+                          phone=phone, message=message, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent')
+
     return render(request, 'contact.html')
     # return HttpResponse("This is contact page")
