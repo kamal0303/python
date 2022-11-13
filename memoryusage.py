454a73e81faf9cff89b425015552407764808cfe
kamal0303 <kamalsharma0308@gmail.com>
2022-11-13 13:04:23 +0530

    update

---
+++ memoryusage.py
@@ -0,0 +1,9 @@
+import sys
+l = [1, 2, 3, 4, 5]
+t = (1, 2, 3, 4, 5)
+s = "a, b, c, d , e"
+
+print(sys.getsizeof(l))
+print(sys.getsizeof(t))
+print(sys.getsizeof(s))
+print(s)
---
+++ prac_sc.py
@@ -0,0 +1,13 @@
+# l = ["kamal", 10.2, 1988]
+# for s in l:
+#     print(s)
+
+# counter variable
+# while (test expression)
+# statement
+#increment / decrement
+
+counter = 1
+while counter <= 4:
+    print('a')
+    counter = counter + 1
