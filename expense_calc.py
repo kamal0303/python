4af6a20a76b9fa301624f9f5d15630fbe1389e4c
kamal0303 <kamalsharma0308@gmail.com>
2022-11-13 14:28:24 +0530

    update

---
+++ expense_calc.py
@@ -0,0 +1,16 @@
+exp = -1
+total = 0
+maxexp = 0
+minexp = 0
+
+
+while exp != 0:
+    exp = int(input("what is the expense? (type 0 to stop)"))
+    total = total + exp
+    if exp > maxexp:
+        maxexp = exp
+        # minexp = min(exp)
+
+    print("your total expenditure is " + str(total))
+    print("the maximum you spend is " + str(maxexp))
+    # print("the minimum you spend is " + str(minexp))
--- prac_sc.py
+++ prac_sc.py
@@ -11,3 +11,12 @@ counter = 1
 while counter <= 4:
     print('a')
     counter = counter + 1
+
+x = 10
+y = 10.0
+if x == 10:
+    print("x is equal to 10")
+if x > 5:
+    print("x is greate than 5")
+if x == y:
+    print("x is equal to y")
